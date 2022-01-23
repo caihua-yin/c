7f9160846f341e27992b4e6cfe5d45769d769490
caihua.ych <caihua.ych@alibaba-inc.com>
2022-01-23 15:57:10 +0800

    Add Roman to inmplementation

---
+++ leetcode/romantoint/main.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+
+typedef enum
+{
+    // Q stands for invalid roman number
+    Q = -1,
+
+    // Valid roman number starts here
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000
+} Roman;
+
+Roman char2Roman(char c)
+{
+    switch(c)
+    {
+        case 'I':
+            return I;
+        case 'V':
+            return V;
+        case 'X':
+            return X;
+        case 'L':
+            return L;
+        case 'C':
+            return C;
+        case 'D':
+            return D;
+        case 'M':
+            return M;
+        default:
+            return Q;
+    }
+}
+
+int roman2int(const char *c)
+{
+    // The intermediate result can be negative, e.g. IV
+    short result = 0;
+    unsigned short n = 0 ;
+    unsigned short next = 0;
+    while (*c != '\0')
+    {
+        // Optimize this line to be n = next;
+        n = char2Roman(*c);
+
+        // Handle invalid case
+        if (n < 0)
+           return n;
+
+        if (*(c+1) != '\0')
+        {
+            next = char2Roman(*(c+1));
+            if (n < next)
+                result -= n;
+        }
+        result += n;
+
+        // Advance c
+        c++;
+    }
+
+    return result;
+}
+
+int main()
+{
+    printf("III[3] is %d\n", roman2int("III"));
+    printf("LVIII[58] is %d\n", roman2int("LVIII"));
+    printf("MCMXCIV[1994] is %d\n", roman2int("MCMXCIV"));
+    return 0;
+}
